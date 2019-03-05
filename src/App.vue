b979ca87860ad0a210befcc6e5981a67c425e2c6
木村幸 <kimuramiyuki@kimuramiyukinoMacBook-Pro.local>
2019-03-05 18:16:37 +0900

    apiと接続するてまえ

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,48 @@
 <template>
   <div id="app">
     <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet">
-    <div class = "header">
+    <header>
       <p class = "wf-nicomoji">#のぎまに。</p>
       <a>毎週月曜日更新！乃木坂46の握手会神対応ランキング！(流れる)</a>
-    </div>
+      {{ results }}
+    </header>
     <div class = "rank">
       <p class = "wf-nicomoji">こんしゅうのランキング</p>
       <div class="prof">
         <a class = "rank-top">１位  山下美月</a><br>
-        <img src = "https://48pedia.org/images/8/87/2018%E5%B9%B4%E4%B9%83%E6%9C%A8%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E5%B1%B1%E4%B8%8B%E7%BE%8E%E6%9C%88_2.jpg" alt="山下美月">
-      </div>
-      <div class = "prof ppl">
-        <a class = "rank-top">２位　秋元真夏</a><br>
-        <img src = "https://48pedia.org/images/8/85/2018%E5%B9%B4%E4%B9%83%E6%9C%A8%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E7%A7%8B%E5%85%83%E7%9C%9F%E5%A4%8F_2.jpg" alt="秋元真夏">
-      </div>
-      <div class="prof">
-        <a class = "rank-top">３位　山下美月</a><br>
-        <img src = "https://48pedia.org/images/8/87/2018%E5%B9%B4%E4%B9%83%E6%9C%A8%E5%9D%8246%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB_%E5%B1%B1%E4%B8%8B%E7%BE%8E%E6%9C%88_2.jpg" alt="山下美月">
+        <img src = "images/山下美月.jpg" alt = "山下美月">
       </div>
       <div class = "prof ppl">
         <a>４位  木村みゆき</a><br>
       </div>
-      <div class = "prof">
-        <a>５位  みゆき木村</a><br>
-      </div>
-      <div class = "prof ppl">
-        <a>６位  みゆき</a><br>
-      </div>
-      <div class = "prof">
-        <a>７位  あられ</a><br>
-      </div>
-      <div class = "prof ppl">
-        <a>８位  お腹痛い</a><br>
-      </div>
-      <div class = "prof">
-        <a>９位  ダム</a><br>
-      </div>
-      <div class = "prof ppl">
-        <a>１０位  カラオケ</a><br>
-      </div>
       <a>もっとみる▼</a>
     </div>
-    <div class = "footer">
+    <footer>
       <a>ページトップへ</a><br>
       <a><small>copyright</small> みゆき</a>
-    </div>
+    </footer>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      results: []
+    }
+  },
+  methods:{
+    mounted(){
+      axios.get('http://localhost:5000/results')
+      .then((res) => {
+        console.log(res.data)
+        this.results = res.data
+      })
+      .catch()
+    }
+  }
 }
 </script>
 
@@ -70,14 +61,14 @@ a{
 }
 
 /* ヘッダー */
-.header .wf-nicomoji {
+header .wf-nicomoji {
   font-family: "Nico Moji";
   font-size: 75px;
   color: rgba(215, 47, 252, 1);
   margin-left: 9.4px;
 }
 
-.header p{
+header p{
   margin: 0px;
 }
 
@@ -90,7 +81,6 @@ a{
 }
 
 .rank a {
-  font-size: 25px;
   position: relative;
   padding: 0 20px;
   margin: 10px;
@@ -114,7 +104,7 @@ a{
   padding: 5px;
 }
 
-.prof img{
+.rank .prof img{
   width: 50%;
   height: auto;
   margin-bottom: 10px;
@@ -125,11 +115,10 @@ a{
 }
 
 /* フッター */
-.footer{
+footer{
   height: 50px;
   background-color: rgba(215, 47, 252, 1);
   bottom: 0;
 }
-
 
 </style>
